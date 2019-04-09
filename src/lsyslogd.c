e97e2e30d67e82983c556b1bd26a379918652cd9
Robin <robin@chilio.net>
2019-04-09 15:09:31 +0200

    lsyslogd: Make register output function public

--- src/lsyslogd.c
+++ src/lsyslogd.c
@@ -25,27 +25,31 @@ syslog_input_t *input;
 syslog_ouput_t outputs[MAX_OUTPUTS];
 int curr = 0;
 
-static int register_output(syslog_init_f _init, syslog_write_f _write) {
-    printf("add output %d\n", curr);
-    outputs[curr]._init = _init;
-    outputs[curr]._write = _write;
-    curr++;
-    return curr;
-}
-
 static syslog_input_t *register_input(syslog_input_t *i) {
     input = i;
     return input;
+}
+
+int register_output(syslog_init_f _init, syslog_write_f _write) {
+    if(curr < MAX_OUTPUTS) {
+        outputs[curr]._init = _init;
+        outputs[curr]._write = _write;
+        curr++;
+        return curr;
+    }
+    return -1;
 }
 
 int lsyslogd_init(syslog_input_t *input, char *opt) {
     register_input(input);
     input->_init(opt);
     memset(outputs, 0, sizeof(outputs));
+
     #ifdef FEATURE_OUTPUT_STDOUT
     register_output(syslog_output_stdout_init, syslog_output_stdout_write);
     syslog_output_stdout_init(NULL);
     #endif
+
     return 0;
 }
 
@@ -59,7 +63,7 @@ int lsyslogd_run(void) {
             return read;
         }
         for (int i = 0; i<curr;i++) {
-            outputs[i]._write(log_buf, read);
+            outputs[i]._write(log_buf,read);
         }
     }
 }
